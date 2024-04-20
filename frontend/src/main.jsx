ca1cf832fccbd0d16437763052c14aca655d89eb
Gabriel Gibb <gabrielgibb1@gmail.com>
2024-04-20 15:53:32 -0700

    update

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+)
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+
+export default function Login() {
+
+
+    return (
+        <div data-theme="light" className="h-screen flex flex-col">
+            <div>
+                login
+            </div>
+        </div>
+    )
+}
