222ba42a43fc0901e2ab43b179d3227137accf2e
kellyserafico <114453810+kellyserafico@users.noreply.github.com>
2024-04-20 16:54:31 -0700

    test

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,9 @@ export default function Home() {
     });
 
     return (
-        <div data-theme="light" className="h-screen flex flex-col">
+        <div data-theme="light" className="h-screen w-screen flex flex-col">
             <div className="m-auto text-center">
-                <img src="home_cat.png" className="m-auto h-1/2" alt="splash"></img>
+                <img src="home_cat.png" className="m-auto w-64" alt="splash"></img>
                 <h1 className="text-4xl mb-6">Welcome to memotrail</h1>
                 <h2 className="text-base font-Poppins mb-14">leave a piece of your heart wherever you go</h2>
                 <button className="btn rounded-full bg-light-green w-72">
