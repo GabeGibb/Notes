5b3d44e3da35592056860b787996eea5b0716dcb
Gabriel Gibb <gabrielgibb1@gmail.com>
2024-04-20 16:35:30 -0700

    save

--- frontend/public/explore.js
+++ frontend/public/explore.js
@@ -7,16 +7,19 @@ async function addEntities(lat, lon) {
     const scene = document.querySelector('a-scene');
     
     // Make a GET request
-    const response = await fetch(`https://api.example.com/endpoint?lat=${lat}&lon=${lon}`);
+    const response = await fetch(`http://127.0.0.1:5000/notes?latitude=-1&longitude=1.0001`);
     const data = await response.json();
-
+    console.log(data)
     // Use the data to add entities to the scene
-    data.entities.forEach(entity => {
+    for (let i = 0; i < data.notes.length; i++) {
+        let note = data.notes[i];
         const entityEl = document.createElement('a-text');
-        entityEl.setAttribute('position', {x: 5, y: 2, z: 5});
-        entityEl.setAttribute('value', entity.text);
+        // entityEl.setAttribute('position', `${note.latitude_distance_feet} 2 ${note.longitude_distance_feet}`);
+        // entityEl.setAttribute('value', note.content);
+        // entityEl.setAttribute('look-at', '[camera]');
         scene.appendChild(entityEl);
-    });
+    }
+    console.log(scene)
 
     // Update the coordinates display
     document.getElementById('coordinates').textContent = `Lat: ${lat.toFixed(5)}, Lon: ${lon.toFixed(5)}`;
@@ -36,20 +39,11 @@ function updateLocation() {
     });
 }
 
-// window.onload = function () {
-//     if ("geolocation" in navigator) {
-//         updateLocation();
-//     } else {
-//         console.log("Geolocation is not supported by this browser.");
-//     }
-// };
-function handleOrientation(event) {
-    const absolute = event.absolute;
-    const alpha = event.alpha;
-    const beta = event.beta;
-    const gamma = event.gamma;
-    console.log(absolute, alpha, beta, gamma)
-    // Do stuff with the new orientation data
-  }
+window.onload = function () {
+    if ("geolocation" in navigator) {
+        updateLocation();
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+};
 
-window.addEventListener("deviceorientation", handleOrientation, true);
