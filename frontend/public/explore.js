4d1af878fc042b5c1cab0da0ecc2a88491de1f88
Gabriel Gibb <gabrielgibb1@gmail.com>
2024-04-20 21:46:59 -0700

    test

--- frontend/public/explore.js
+++ frontend/public/explore.js
@@ -4,45 +4,41 @@ var initialLocationsSet = false;
 
 
 async function addEntities(lat, lon) {
-    const scene = document.getElementById('scene');
+    const scene = document.querySelector("a-scene");
 
     // Make a GET request
     const response = await fetch(`http://127.0.0.1:5000/notes?latitude=${lat}&longitude=${lon}`);
     const data = await response.json();
-    console.log(lat, lon)
     console.log(data)
+    htmlContent = ''
+    for (let i = 0; i < data.notes.length; i++) {  
+        let note = data.notes[i];
+        let entity = document.createElement('a-plane');
+        let randomX = Math.floor(Math.random() * 50);
+        let randomZ = Math.floor(Math.random() * 50);
+        console.log(randomX, randomZ)
+        entity.setAttribute('position', `${randomX} 1 ${randomZ}`);
+        entity.setAttribute('position', '15 1 -18');
+        entity.setAttribute('rotation', '0 0 0');
+        entity.setAttribute('width', '4');
+        entity.setAttribute('height', '4');
+        entity.setAttribute('material', 'src: home_cat.png; transparent: true');
+        entity.setAttribute('read-note', '');
+        entity.setAttribute('cursor-listener', '');
 
-    // let htmlContent = `<a-scene embedded class="fullscreen" vr-mode-ui="enabled: false" arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false" renderer="antialias: true; alpha: true">
-    // <a-box position="-1 0.5 -30" rotation="0 45 0" color="#4CC3D9"></a-box>
-    // `
+        scene.appendChild(entity)
 
-    // //   <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-    // //   <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-    // //   <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-    // //   <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+      }
+    var div = document.createElement('div');
+    div.innerHTML = htmlContent;
+    scene.appendChild(div);
+    console.log(scene)
 
-
-    // for (let i = 0; i < data.notes.length; i++) {  
-    //     let note = data.notes[i];
-    //     let entity = `
-    //         <a-box
-    //         position="${note.longitude_distance_feet} 0 ${note.latitude_distance_feet}"
-    //         rotation="0 45 0" color="#4CC3D9">
-    //         </a-box>
-    //     `;
-    //     console.log(entity)
-    //     htmlContent += entity;
-
-    //   }
-    //   htmlContent += `</a-scene>`;
-    // scene.innerHTML = htmlContent;
-    // console.log(scene)
-    // Update the coordinates display
     document.getElementById('coordinates').textContent = `Lat: ${lat.toFixed(5)}, Lon: ${lon.toFixed(5)}`;
 }
 
 function updateLocation() {
-    navigator.geolocation.watchPosition(function (position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
         addEntities(latitude, longitude);
